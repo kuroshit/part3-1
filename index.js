3e6bca96dc7ef70924a7be108cd8e52a2010c34f
Barkhasbadi Ankhbileg <barhas16@gmail.com>
2024-12-19 22:35:15 +0800

    frontend prod build

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("body", (req) => JSON.stringify(req.body));
 
+app.use(express.static("dist"));
+app.use(cors());
 app.use(express.json());
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body', {
-    skip: (req) => req.method !== "POST",
-  })
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :body"
+    // {
+    //   skip: (req) => req.method !== "POST",
+    // }
+  )
 );
 
 let persons = [
@@ -33,6 +39,63 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: String(notes.length + 1),
+    content: body.content,
+    date: new Date(),
+    important: body.important || false,
+  };
+
+  notes = notes.concat(note);
+
+  res.json(note);
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const body = req.body
+
+  console.log(body)
+
+  notes = notes.map(note => note.id !== id ? note : body)
+
+  res.json(body)
+})
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -96,6 +159,6 @@ app.delete("/api/notes/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
