4fffc7e61ad856d62c6aab2d605199abf479306d
Barkhasbadi Ankhbileg <barkhasbadi.a@mbank.local>
2024-12-26 15:40:00 +0800

    project structure update

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': [
+        'error',
+        'always'
+      ],
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error',
+        'always'
+      ],
+      'arrow-spacing': [
+        'error',
+        {
+          before: true,
+          after: true
+        }
+      ],
+      'no-console': 0,
+    },
+    ignores: [
+      "node_modules",
+      "dist",
+      'build',
+    ],
+  },
+];
